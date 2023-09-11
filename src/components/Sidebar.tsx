2956e7915c477e4264be15a291242af167d73046
szy-kenn <johnkennethvelano@gmail.com>
2023-09-11 20:12:25 +0800

    started styling sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,23 +1,28 @@
-import React from "react";
-
 const Sidebar = ({}) => {
     return (
         <div className="sidebar-container">
-            <h1 className="sidebar-header">Sidebar Header</h1>
-            <div className="category">
-                <h1 className="category-name">Category #1</h1>
-                <div className="category-options-container">
-                    <div className="category-options">option 1</div>
-                    <div className="category-options">option 2</div>
-                    <div className="category-options">option 3</div>
+            <h1 className="title">Nexus</h1>
+            <div className="sidebar-contents">
+                <h1 className="sidebar-header">Filter Items</h1>
+                <div className="category">
+                    <h1 className="category-name">Category #1</h1>
+                    <div className="category-options-container">
+                        <label htmlFor="options1">Option #1</label>
+                        <input
+                            type="checkbox"
+                            name="options1"
+                            id="options1"
+                            className="category-options"
+                        />
+                    </div>
                 </div>
-            </div>
-            <div className="category">
-                <h1 className="category-name">Category #1</h1>
-                <div className="category-options-container">
-                    <div className="category-options">option 1</div>
-                    <div className="category-options">option 2</div>
-                    <div className="category-options">option 3</div>
+                <div className="category">
+                    <h1 className="category-name">Category #1</h1>
+                    <div className="category-options-container">
+                        <div className="category-options">option 1</div>
+                        <div className="category-options">option 2</div>
+                        <div className="category-options">option 3</div>
+                    </div>
                 </div>
             </div>
         </div>
